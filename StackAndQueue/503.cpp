8133443f080745ac7298fa6f8d1fe42f6fc47482
Lalit Wagh <lalitwagh2804@gmail.com>
2026-05-16 23:57:42 +0530

    Next Greater-II

---
+++ StackAndQueue/503.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/next-greater-element-ii/submissions/2002648730/
+
+#include<bits/c++io.h>
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>res(n ,-1);
+        stack<int>st;
+        for(int i= 2*n-1 ; i>= 0 ;i--){
+            int index = i%n ;
+            int curr = nums[index];
+
+            while(!st.empty() && st.top() <= curr){
+                st.pop();
+            }
+            if(i<n){
+                if(st.empty()){
+                    res[i] = -1;
+                }else{
+                    res[i] = st.top();
+                }
+            }
+            st.push(curr);
+        }
+        return res;
+    }
+};
