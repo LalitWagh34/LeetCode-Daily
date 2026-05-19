8fee6274b6bee49a514474d78c79aa226a57b9a4
Lalit Wagh <lalitwagh2804@gmail.com>
2026-05-19 17:41:53 +0530

    Domino_piling

---
+++ competitve/Domino_piling.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int m ;
+    int n ;
+    cin >> m >> n;
+
+    int rec_board = m*n ;
+
+    int ans = rec_board/2;
+
+    cout << ans;
+}
