19571d9d0170fa093631623024cabd07daee1824
Lalit Wagh <138019530+LalitWagh34@users.noreply.github.com>
2026-09-05 12:30:23 +0530

    [210] [Time Beats: 38.89%] [Memory Beats: 51.91%] - LeetPush

--- 210-Course-Schedule-II.cpp
+++ 210-Course-Schedule-II.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
-    bool dfs(int node, vector<int>& visPath, vector<int> adj[],
-             vector<int>& vis ,vector<int>&findOrder) {
-        vis[node] = 1;
-        visPath[node] = 1;
-        for (auto adjNode : adj[node]) {
-            if (!vis[adjNode]) {
-                if (dfs(adjNode, visPath, adj, vis ,findOrder) == true)
-                    return true;
-            } else if (visPath[adjNode]) {
-                return true;
-            }
-        }
-        // check[node] =1;
-        findOrder.push_back(node);
+    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
+        vector<int>indegree(V ,0);
+        vector<int>adj[V];
 
-        visPath[node] = 0;
-        return false;
-    }
-    vector<int> findOrder(int V, vector<vector<int>>& edges) {
-        vector<int> adj[V];
-        for (auto it : edges) {
+        for(auto it :prerequisites){
             adj[it[1]].push_back(it[0]);
-            
         }
 
-        vector<int> vis(V, 0);
-        vector<int> visPath(V, 0);
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
 
-        vector<int>check(V,0);
-        vector<int>findOrder;
-        for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                if (dfs(i, visPath, adj, vis , findOrder) ){
-                    return {};
-                }
-    
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            topo.push_back(node);
+
+            for(auto it :adj[node]){
+                indegree[it]--; 
+                if(indegree[it] ==0) q.push(it);
             }
         }
 
+        if(topo.size() ==V) return topo ;
+        return{};
 
-        reverse(findOrder.begin(), findOrder.end());
-        return findOrder;
     }
 };
